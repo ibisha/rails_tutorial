81df521971ed7bef2c3e5ea00793d6bc6785d539
ibisha <akashiroakaaka@gmail.com>
2017-05-22 23:18:30 +0900

    Add authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticte(params[:session][:password])
-      # login
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
+      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
       render 'new'
     end
   end
