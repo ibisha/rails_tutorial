24b18a7b4b6982f29f058eaa511bc04a6dc1f379
ibisha <akashiroakaaka@gmail.com>
2017-05-22 23:06:37 +0900

    Add authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticte(params[:session][:password])
+      # login
+    else
+      render 'new'
+    end
   end
 
   def destroy
