e33207c51ffe36f3ca5d8b6e2cb61ea0d505af40
ibisha <akashiroakaaka@gmail.com>
2017-05-27 18:17:13 +0900

    Layout change if user is login or not

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+
+  # 渡されたユーザでログインして、セッションに保存する
+  # param ユーザ
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在ログイン中のユーザを返す
+  # ログインしていなければnil
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ユーザがログインしているかどうかを返す
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
