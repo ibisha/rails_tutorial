531607fe00512b045f84adbeee761917f4ed4433
ibisha <akashiroakaaka@gmail.com>
2017-06-10 23:22:23 +0900

    Account activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,17 @@
 class AccountActivationsController < ApplicationController
+
+  # アカウントを有効化する
+  def edit
+    user = User.find_by(email: params[:email])
+    # ユーザが無効状態の場合
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.activate
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid acitivation link"
+      redirect_to root_url
+    end
+  end
 end
